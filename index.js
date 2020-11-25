f7ca274992e51c8b5815b08ff4078a15bde8de1f
Kenan Dolic <kdolic@alumni.iastate.edu>
2020-11-25 16:14:59 -0600

    Only two tasks left

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 TASK 1 🚀
 // in your own words explain what a closure is below in comments and then write an example of a closure. Try to make this explaination simple enough to explain to a younger sibling. */
 
+// combination of a function within another function (enclosed together), gives you acces to an outer function scope from an inner. Something nested inside of another thing.
 
+const external = "I'm outside the function";
 
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
 
+  function nestedFunction() {
+    console.log(internal);
+  }
+  nestedFunction();
+}
+myFunction();
+
+// The function called nestedFunction is still wrapped inside of the parent function which is myFunction and has access to that variable called internal
 
 
 /*
 TASK 2 🚀
-// look at the code below and explain in your own words where the variable 'count' is available. 
-// Explain why 'count' is initialized with a let and not a var or const. 
-// Explain how initalizing the variable 'count' with a var would change it's scope
+// look at the code below and explain in your own words where the variable 'count' is available. - it is available locally to counterMaker and within counter function. counter function can go outside of its scope to access the variable 
+// Explain why 'count' is initialized with a let and not a var or const. - typically we dont use var anymore because of ES6, and const would not make sense because it will constantly change when incremented. Const means it would stay as the same.
+// Explain how initalizing the variable 'count' with a var would change it's scope - var would allow it to change throughout any scope and can be chaotic, AVOID var
 */
 function counterMaker() {
     let count = 0;
@@ -30,10 +43,10 @@ TASK 3 🚀
 * The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  Window Binding - not something that is supposed to be used. If we provide "this" then it will go to the window or global object, unless in strict mode(will be undefined)
+* 2.  Implicit Binding - most common/ highly used, after the function is invoked, look left of the fot to see what what the "this" is refering to, only for object with methods inside
+* 3.  Explicit Binding - three ways: .call(), .apply(), .bind().. .call you pass in your arguments 1 by 1, .apply you pass in your arguments as an array, .bind() returns a new func
+* 4.  New Binding - using the "new" keyword to construct a new object and "this" points directly to that object, when invoked the "this" within the new keyword points to that object
 *
 * write out a code example of each explanation above
 */
@@ -42,18 +55,44 @@ TASK 3 🚀
 
 // code example for Window Binding
 
+// Your global context depends on where you're working. If you're in the browser, this will be the window. When programming in strict mode, the global context is undefined.
+
+// For example while in Chrome: function printMe = function () { console.log(this); } printMe() // prints your 'Window Object' if in the browser!
+
 // Principle 2
 
 // code example for Implicit Binding
+
+// var MyObject = function (){
+//   this.name = 'MyObjectName';
+//   this.myProperty = 'property';
+// };
+
+// MyObject.prototype.doStuff = function (action) {
+//   console.log(this.name + ' is ' + action + '!');
+// }
+
+// var obj = new MyObject();
+
+// obj.doStuff('awesome'); // prints 'MyObjectName is awesome!'
 
 // Principle 3
 
 // code example for New Binding
 
+// function newBinding(a){
+//   this.a = a;
+//  }
+//  var newlyCreated = new newBinding(10);
+//  console.log(newBinding.a)//10;
+//  calling new in front of newBinding, we have constructed a new object and set that new object as the "this" for the call of newBinding.
+
 // Principle 4
 
 // code example for Explicit Binding
 
+// var runner = { name: 'John', myFavoriteActivity: 'running' };
+//   MyObject.prototype.doStuff.call(runner, runner.myFavoriteActivity); // prints 'John is running!';
 
 
 
